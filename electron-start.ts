10c42d447634de8c80ae46b429eba8d45a6d3623
Dillon Kearns <dillon@dillonkearns.com>
2019-12-06 04:36:16 -0800

    Update electron-builder config to hide icon, add double click to quit.

--- electron-start.ts
+++ electron-start.ts
@@ -1,34 +1,49 @@
 // const {app, globalShortcut, dialog, BrowserWindow, ipcMain, Tray} = require('electron')
 import {exec} from 'child_process';
 import {app, globalShortcut, dialog, BrowserWindow, ipcMain, Tray}  from 'electron';
-import * as moment from 'moment';
-const path = require('path')
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is GCed.
+import * as path from 'path';
 
-// Quit when all windows are closed.
-app.on('window-all-closed', function() {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform != 'darwin') {
-    app.quit();
-  }
-});
+
+const assetsDirectory = path.join(__dirname, 'assets');
+
 let tray: Tray;
 let timeLeft = 25 * 60;
 
-
+let audioPlayer = exec('ls');
 function startTimer() {
+  // const rainMp3 = path.join(assetsDirectory, 'rain.mp3')
+  exec(`osascript -e 'tell application "Spotify" to play'`);
+  const rainMp3 = '/Users/dillonkearns/src/github.com/dillonkearns/tempo/assets/rain.mp3'
   
-  exec('afplay ./rain.mp3', function() {})
+  
+  // exec(rainMp3, (err, stdout, stderr) => {
+  //   if (err) {
+  //     dialog.showMessageBox({
+  //       type: 'info',
+  //       message: 'Error!',
+  //       detail: `${err}`,
+  //       buttons: ['OK']
+  //     });
+  
+  //     console.error(err);
+  //     return;
+  //   }
+  //   console.log(stdout);
+  // });
+  audioPlayer = exec(`afplay ${rainMp3}`, function() {});
+  app.on('will-quit', function () {
+    audioPlayer.kill();
+  });
   timeLeft = 25 * 60;
-  setInterval(() => {
-    tray.setTitle(secondsToTime(timeLeft));
-    timeLeft--;
-  }, 1000);
+  // timeLeft = 10;
+  tickTimer();
+  // setInterval(() => {
+  //   tray.setTitle(secondsToTime(timeLeft));
+  //   timeLeft--;
+  // }, 1000);
   
   // tray.on('right-click', toggleWindow)
-  // tray.on('double-click', toggleWindow)
+  tray.on('double-click', app.quit)
   // tray.on('click', function (event) {
   //   toggleWindow()
   //
@@ -39,11 +54,22 @@ function startTimer() {
   // })
 }
 
+function tickTimer() {
+    timeLeft--;
+    if (timeLeft > 0) {
+      tray.setTitle(secondsToTime(timeLeft));
+      setTimeout(tickTimer, 1000);
+    } else {
+      tray.setTitle('');
+      audioPlayer.kill();
+      exec(`osascript -e 'tell application "Spotify" to pause'`);
+    }
+}
+
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 app.on('ready', function() {
-  const assetsDirectory = path.join(__dirname, 'assets');
   tray = new Tray(path.join(assetsDirectory, 'tray-icon-small.png'))
   globalShortcut.register('CommandOrControl+Shift+;', startTimer);
 
@@ -57,5 +83,10 @@ function secondsToTime(seconds: number): string {
   let minutesLeft = Math.floor(seconds / 60);
   let secondsLeft = seconds % 60;
 
-  return `${minutesLeft}:${secondsLeft}`
+  // return `${minutesLeft}:${secondsLeft}`
+  return str_pad_left(minutesLeft,'0',2)+':'+str_pad_left(secondsLeft,'0',2);
+}
+
+function str_pad_left(string: number,pad: string,length: number) {
+  return (new Array(length+1).join(pad)+string).slice(-length);
 }
